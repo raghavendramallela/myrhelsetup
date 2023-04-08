3e3184623c04d0fe7c41ccffb120b62a99797c6d
raghu <58185871+raghavendramallela@users.noreply.github.com>
2023-04-09 04:04:39 +0530

    updated new nv driver

--- nvidia-installation-legion5i/1-nvidiadownload.sh
+++ nvidia-installation-legion5i/1-nvidiadownload.sh
@@ -1,7 +1,9 @@
 cd ~/Downloads/
 
 echo "Downloading NVIDIA run script..."
-wget https://download.nvidia.com/XFree86/Linux-x86_64/515.76/NVIDIA-Linux-x86_64-515.76.run
+#wget https://download.nvidia.com/XFree86/Linux-x86_64/515.76/NVIDIA-Linux-x86_64-515.76.run
+wget https://download.nvidia.com/XFree86/Linux-x86_64/525.105.17/NVIDIA-Linux-x86_64-525.105.17.run
+
 
 echo "changing nvidia run permissions..."
 chmod +x ~/Downloads/NVIDIA-Linux*.run
